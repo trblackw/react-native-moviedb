98461b1c34bd5fafc4d0958678fc56803fa73c48
Tucker <trblackw@ncsu.edu>
2020-02-23 10:39:45 -0500

    Add various types for movie odbapi

--- types.ts
+++ types.ts
@@ -1,7 +1,39 @@
-export interface Movie {
+export interface MovieOverview {
     Title: string;
     Year: string;
     imdbID: string;
     Type: string;
     Poster: string;
+}
+
+export interface MovieDetails {
+    Title: string;
+    Year: string;
+    Rated: string;
+    Released: string;
+    Runtime: string;
+    Genre: string;
+    Director: string;
+    Writer: string;
+    Actors: string;
+    Plot: string;
+    Language: string;
+    Country: string;
+    Awards: string;
+    Ratings: Rating[];
+    Metascore: string;
+    imdbRating: string;
+    imdbVotes: string;
+    imdbID: string;
+    Type: string;
+    DVD: string;
+    BoxOffice: string;
+    Production: string;
+    Website: string;
+    Response: string;
+}
+
+interface Rating {
+    Source: string;
+    Value: string;
 }
